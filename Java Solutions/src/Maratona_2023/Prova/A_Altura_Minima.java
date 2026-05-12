630a556dab363cc73132bb0edc9c0f632eb4d902
Pedr0fsc <pedrodafonsecaschwertner@gmail.com>
2026-05-12 17:58:13 -0300

    2023 exercises

---
+++ Java Solutions/src/Maratona_2023/Prova/A_Altura_Minima.java
@@ -0,0 +1,38 @@
+package Maratona_2023.Prova;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class A_Altura_Minima {
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        try {
+            File myFile = new File("Java Solutions/input.txt");
+            Scanner reader = new Scanner(myFile);
+
+            int n = reader.nextInt();
+            int altura =  reader.nextInt();
+            int contador = 0;
+
+            for(int i = 0; i < n; i++){
+                int k = reader.nextInt();
+                if(k <= altura) contador++;
+            }
+
+            System.out.println(contador);
+
+            reader.close();
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("The file 'input.txt' was not found.");
+        }
+
+        long endTime = System.nanoTime();
+        // Convertendo nanosegundos para milissegundos
+        double duration = (endTime - startTime) / 1_000_000.0;
+        System.out.println("Execution Time: " + duration);
+    }
+}
---
+++ Java Solutions/src/Maratona_2023/Prova/L_Lexicograficamente_Agradavel.java
@@ -0,0 +1,50 @@
+package Maratona_2023.Prova;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class L_Lexicograficamente_Agradavel {
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        try {
+            File myFile = new File("Java Solutions/input.txt");
+            Scanner reader = new Scanner(myFile);
+
+            String str = reader.nextLine();
+            int n = reader.nextInt();
+
+            reader.close();
+
+            int limit = str.length() - n;
+            char[] strChar = str.toCharArray();
+            int contador;
+
+            do {
+                contador = 0;
+                for (int i = 0; i < limit; i++) {
+                    if(strChar[i] > strChar[i+n]) {
+                        char temp = strChar[i];
+                        strChar[i] = strChar[i + n];
+                        strChar[i + n] = temp;
+                        contador++;
+                    }
+                }
+            } while (contador > 0);
+
+            str = new String(strChar);
+            System.out.println(str);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("The file 'input.txt' was not found.");
+        }
+
+
+
+        long endTime = System.nanoTime();
+        // Convertendo nanosegundos para milissegundos
+        double duration = (endTime - startTime) / 1_000_000.0;
+        System.out.println("Execution Time: " + duration);
+    }
+}
